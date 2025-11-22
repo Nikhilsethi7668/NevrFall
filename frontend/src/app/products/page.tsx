0dd34a52a34d4eff43d9848ef0d1d549b1dbb074
Rishabh Tyagi <tyagirishabh121@gmail.com>
2025-11-23 02:53:43 +0530

    Re-render issue resolved.

--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -45,7 +45,7 @@ function ProductsPageContent() {
     }
     router.push(`${window.location.pathname}?${query.toString()}`);
     fetchProducts(filters, page);
-  }, [filters, page]);
+  }, [filters]);
 
   // --- Fetch facets for dynamic filters ---
   const { data: facetsData } = useQuery({
